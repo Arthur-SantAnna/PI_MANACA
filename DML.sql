de5469bf8f2438b4b27939a4be2769c806b2397f
Gabriel Claudino <gaclaudino.13@gmail.com>
2022-11-11 23:16:10 -0300

    Create DML.sql

---
+++ DML.sql
@@ -0,0 +1,5 @@
+-- Tabela Cliente
+INSERT INTO tb_cliente VALUES (sq_cliente.nextval, 'Anthony Edward Stark', '234.654.872-21', 'Rico13', 'stark@gmail.com');
+INSERT INTO tb_cliente VALUES (sq_cliente.nextval, 'Robert Pattinson', '164.832.567-01', 'Bella', 'pattinson.10@hotmail.com');
+
+-- Tabela Container

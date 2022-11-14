ddc3a6fd56b7666c4ae5956757e1062eda65d26f
Arthur Sant'Anna <84470146+Arthur-SantAnna@users.noreply.github.com>
2022-11-13 23:15:41 -0300

    Update trigger_procedure.sql

--- trigger_procedure.sql
+++ trigger_procedure.sql
@@ -4,31 +4,31 @@ for each row
 
 begin
         if(:old.cor_luz <> :new.cor_luz) then
-            insert into tb_log_cor_luz values(sq_log_cor_luz.nextval,:old.id_co,:old.cor_luz,SYSDATE);
+            insert into tb_log_cor_luz values(sq_log_cor_luz.nextval,:old.id_co,:new.cor_luz,SYSDATE);
         end if;
         
         if(:old.periodo_irrigacao <> :new.periodo_irrigacao) then
-            insert into tb_log_periodo_irrigacao values(sq_log_periodo_irrigacao.nextval,:old.id_co,:old.periodo_irrigacao,SYSDATE);
+            insert into tb_log_periodo_irrigacao values(sq_log_periodo_irrigacao.nextval,:old.id_co,:new.periodo_irrigacao,SYSDATE);
         end if;
         
         if(:old.temperatura <> :new.temperatura) then
-            insert into tb_log_temperatura values(sq_log_temperatura.nextval,:old.id_co,:old.temperatura,SYSDATE);
+            insert into tb_log_temperatura values(sq_log_temperatura.nextval,:old.id_co,:new.temperatura,SYSDATE);
         end if;
         
         if(:old.umidade <> :new.umidade) then
-            insert into tb_log_umidade values(sq_log_umidade.nextval,:old.id_co,:old.umidade,SYSDATE);
+            insert into tb_log_umidade values(sq_log_umidade.nextval,:old.id_co,:new.umidade,SYSDATE);
         end if;
         
         if(:old.intensidade_luz <> :new.intensidade_luz) then
-            insert into tb_log_intensidade_luz values(sq_log_intensidade_luz.nextval,:old.id_co,:old.intensidade_luz,SYSDATE);
+            insert into tb_log_intensidade_luz values(sq_log_intensidade_luz.nextval,:old.id_co,:new.intensidade_luz,SYSDATE);
         end if;
         
         if(:old.intensidade_irrigacao <> :new.intensidade_irrigacao) then
-            insert into tb_log_intensidade_irrigacao values(sq_log_intensidade_irrigacao.nextval,:old.id_co,:old.intensidade_irrigacao,SYSDATE);
+            insert into tb_log_intensidade_irrigacao values(sq_log_intensidade_irrigacao.nextval,:old.id_co,:new.intensidade_irrigacao,SYSDATE);
         end if;
         
         if(:old.capacidade_tanque_atu <> :new.capacidade_tanque_atu) then
-            insert into tb_log_capacidade_tanque_atu values(sq_log_capacidade_tanque_atu.nextval,:old.id_co,:old.capacidade_tanque_atu,SYSDATE);
+            insert into tb_log_capacidade_tanque_atu values(sq_log_capacidade_tanque_atu.nextval,:old.id_co,:new.capacidade_tanque_atu,SYSDATE);
         end if;
     
 end;
